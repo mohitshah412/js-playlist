2c50e76ca29217e9629b036306783b00b68a67e0
mohitshah412 <mohitshah412@gmail.com>
2024-10-10 13:18:43 +0000

    datatypes

---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,25 @@
+"use strict"; // trate all JS code as newr version
+
+// alert(3 + 3) we are using nodejs, not browser
+
+console.log(3 
+    + 
+    3)          // code readability should be high
+console.log("mohit")
+
+let name = "mohit"
+let age = 18
+let isLoggedIn = false
+
+// number => 2 to the power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value , representation of empty value , 
+// undefined =>
+// symbol => unique
+
+// object 
+
+console.log(typeof undefined);      //type is undefine
+console.log(typeof null);           // type is object
