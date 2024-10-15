18473a5f0cbd235eed80aec93e235183c3e92e15
mohitshah412 <mohitshah412@gmail.com>
2024-10-15 07:23:23 +0000

    strings

---
+++ 01_Basics/05_strings.js
@@ -0,0 +1,48 @@
+const name = 'hitesh'
+const repoCount = 50
+
+// console.log(name + repoCount + " value");
+
+console.log(`hello my name is hitesh ${name.toUpperCase()} and my repo count is ${repoCount}`);
+
+const gameName = new String('hitesh-hc-com')
+
+console.log(gameName[0]);
+console.log(gameName.__proto__);
+
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
+
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf('t'));
+
+const newString = gameName.substring(0, 4)
+console.log(newString);
+
+const anotherString = gameName.slice(-8, 4)
+console.log(anotherString);
+
+const newStringOne = "     hitehs.     "
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+const url = "https://hitesh.com/hitesh%20chodhary"
+console.log(url.replace('%20', '-'));
+
+console.log(url.includes('hitesh'));
+
+console.log(gameName.split('-'));
+
+// replaceall()
+// concat()
+// padStart()
+// padEand()
+// charCodeAt()
+
+//**************************** String Method Practice *************************
+
+
+
+
+
+
