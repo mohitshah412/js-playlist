d70f5a845c1b8b482f87d4a960db9ac0698367dc
mohitshah412 <mohitshah412@gmail.com>
2024-12-14 02:33:15 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,31 @@
+"use strict"; // treate all JS code as a newer version
+
+//alert(3 + 3)  // we are using nodejs, not browser
+
+console.log(3
+    + 
+    3)  // code readability should be high
+
+    console.log("hitesh");
+
+let name = "hitesh"
+let age = 18
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => stand alone value
+// undefined => 
+// symbol => unique
+
+
+// object
+
+console.log(typeof "hitesh");
+console.log(typeof age);
+console.log(typeof undefined);
+console.log(typeof null);
+console.log(typeof isLoggedIn);
